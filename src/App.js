fbb32927a28e4aaf4309920c884ea1de3def5ccc
Adebayor <idrisadeebayorr12@gmail.com>
2021-11-25 16:44:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  state = {
+    Time: 0,
+    Person: {
+      fullName: "Adebayo Idris",
+      bio: "I am a young man trying to achieve my goals and desire as a Full-stack software developer. There is way lot to say about myself but if am to keep it short i can say i am pretty honest and a blunt person because i do not hide my feelings from anything or anyone. Life is simple when you live it simple, we live once so i try to get the best out of life before we go 6feet under..",
+      imgSrc: "./image.jpg",
+      profession: "Software developer",
+    },
+    shows: false,
+  };
+
+  openProfile = () => {
+    this.setState({
+      Person: {
+        fullName: "Adebayo",
+        bio: "I am a young man trying to achieve my goals and desire as a Full-stack software developer. There is way lot to say about myself but if am to keep it short i can say i am pretty honest and a blunt person because i do not hide my feelings from anything or anyone. Life is simple when you live it simple, we live once so i try to get the best out of life before we go 6feet under..",
+        imgSrc: "./image.jpg",
+        profession: "Software developer",
+      },
+      shows: true,
+    });
+  };
+
+  closeProfile = () => {
+    this.setState({
+      Person: {
+        fullName: "",
+        bio: "",
+        imgSrc: "",
+        profession: "",
+      },
+      shows: false,
+    });
+  };
+  tick() {
+    this.setState((state) => ({
+      Time: state.Time + 1,
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+  render() {
+    return (
+      <div>
+        <img Src={this.state.Person.imgSrc} alt="" />
+        <h3> {this.state.Person.fullName} </h3>
+        <h3> {this.state.Person.bio} </h3>
+        <h3> {this.state.Person.profession} </h3>
+        <h5> this component was mounted {this.state.Time}s ago</h5>
+        <button
+          onClick={
+            this.state.shows === "true" ? this.openProfile : this.closeProfile
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.state.shows === "true" ? "show" : "hide"}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
